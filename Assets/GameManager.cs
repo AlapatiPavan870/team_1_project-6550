654aa370d97c000765e51f97482efef52b4776d4
Saimounika Bandari <008429402@coyote.csusb.edu>
2024-03-01 23:46:21 -0800

    Added new ScoreCounterScene

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,67 +1,94 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System;
 
-public class GameManager : MonoBehaviour
+public class MathGame : MonoBehaviour
 {
-    public ScoreManager scoreManager;
-    public Text additionProblemText;
+    public Text questionText;
     public Button[] answerButtons;
+    public Text questionCounterText;
 
-    private int operand1;
-    private int operand2;
-    private int correctAnswer;
+    private int questionCounter = 0;
+    private bool quizCompleted = false;
 
     void Start()
     {
-        GenerateAdditionProblem();
+        GenerateQuestion();
     }
 
-    void GenerateAdditionProblem()
+    void GenerateQuestion()
     {
-        operand1 = UnityEngine.Random.Range(1, 10);
-        operand2 = UnityEngine.Random.Range(1, 10);
-        correctAnswer = operand1 + operand2;
-
-        additionProblemText.text = operand1 + " + " + operand2;
-
-        // Shuffle the answers
-        int[] answers = { correctAnswer, correctAnswer + 1, correctAnswer - 1 };
-        Shuffle(answers);
-
-        for (int i = 0; i < answerButtons.Length; i++)
+        if (!quizCompleted)
         {
-            answerButtons[i].GetComponentInChildren<Text>().text = answers[i].ToString();
+            // Increment question counter
+            questionCounter++;
+            if (questionCounter <= 3)
+            {
+                questionCounterText.text = "" + questionCounter +" / 3";
+            }
+            else
+            {
+                questionCounterText.text = ""; // Hide question counter after 3 questions
+            }
+
+            // Generate random numbers for the addition question
+            int num1 = Random.Range(1, 11); // Change the range as per your requirement
+            int num2 = Random.Range(1, 11);
+
+            int answer = num1 + num2;
+
+            // Display the question
+            questionText.text = "" + num1 + " + " + num2 + " ?";
+
+            // Generate random answer options
+            int correctButtonIndex = Random.Range(0, answerButtons.Length);
+
+            for (int i = 0; i < answerButtons.Length; i++)
+            {
+                if (questionCounter <= 3)
+                {
+                    answerButtons[i].gameObject.SetActive(true); // Show answer buttons for first 3 questions
+                }
+                else
+                {
+                    answerButtons[i].gameObject.SetActive(false); // Hide answer buttons after 3 questions
+                }
+
+                if (i == correctButtonIndex)
+                {
+                    answerButtons[i].GetComponentInChildren<Text>().text = answer.ToString();
+                    answerButtons[i].onClick.RemoveAllListeners(); // Remove previous listeners
+                    answerButtons[i].onClick.AddListener(() => CorrectAnswer());
+                }
+                else
+                {
+                    int wrongAnswer = Random.Range(answer + 1, answer + 5); // Change the range as per your requirement
+                    while (wrongAnswer == answer)
+                    {
+                        wrongAnswer = Random.Range(answer + 1, answer + 5);
+                    }
+                    answerButtons[i].GetComponentInChildren<Text>().text = wrongAnswer.ToString();
+                    answerButtons[i].onClick.RemoveAllListeners(); // Remove previous listeners
+                    answerButtons[i].onClick.AddListener(() => WrongAnswer());
+                }
+            }
+
+            if (questionCounter > 3)
+            {
+                quizCompleted = true;
+                questionText.text = "Quiz is completed!";
+            }
         }
     }
 
-
-    void Shuffle<T>(T[] array)
+    void CorrectAnswer()
     {
-        int n = array.Length;
-        while (n > 1)
-        {
-            n--;
-            int k = UnityEngine.Random.Range(0, n + 1);
-            T value = array[k];
-            array[k] = array[n];
-            array[n] = value;
-        }
+        Debug.Log("Correct!");
+        GenerateQuestion();
     }
 
-    public void CheckAnswer(Button button)
+    void WrongAnswer()
     {
-        int selectedAnswer = int.Parse(button.GetComponentInChildren<Text>().text);
-        if (selectedAnswer == correctAnswer)
-        {
-            scoreManager.AddScore();
-            Debug.Log("Correct answer! Score incremented.");
-        }
-        else
-        {
-            Debug.Log("Incorrect answer.");
-        }
-
-        GenerateAdditionProblem();
+        Debug.Log("Wrong!");
+        GenerateQuestion();
     }
 }
